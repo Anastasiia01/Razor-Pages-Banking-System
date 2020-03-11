9aef160500a07dc96261442b6bbfb6d665608e2b
Anastasiia01 <33880873+Anastasiia01@users.noreply.github.com>
2020-03-10 20:45:22 -0400

    Business Layer which applies rules on data
    Calls Data Layer function to get the data from databases and then applies changeable rules on data prior to displaying (e.g. applies a 10% discount on products during Christmas sale).

---
+++ ServicesBusiness/BusinessBanking.cs
@@ -0,0 +1,54 @@
+﻿using BankRPEF.DataLayer;
+using BankRPEF.Models;
+using BankRPEF.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.ServicesBusiness
+{
+    public class BusinessBanking : IBusinessBanking
+    {
+        IRepositoryBanking _irepbank = null;
+        public BusinessBanking(IRepositoryBanking ibank)
+        {
+            _irepbank = ibank;
+        }
+        public BusinessBanking(MYBANKContext context) : this(new RepositoryEF(context))
+        { }
+        public decimal GetCheckingBalance(long checkingAccountNum)
+        {
+            return _irepbank.GetCheckingBalance(checkingAccountNum);
+        }
+        public decimal GetSavingBalance(long savingAccountNum)
+        {
+            return _irepbank.GetSavingBalance(savingAccountNum);
+        }
+        public long GetCheckingAccountNumForUser(string username)
+        {
+            return _irepbank.GetCheckingAccountNumForUser(username);
+        }
+        public long GetSavingAccountNumForUser(string username)
+        {
+            return _irepbank.GetSavingAccountNumForUser(username);
+        }
+        public bool TransferCheckingToSaving(long checkingAccountNum, long savingAccountNum, decimal amount)
+        {
+            return _irepbank.TransferCheckingToSaving(checkingAccountNum, savingAccountNum, amount, 0);
+        }
+        public bool TransferSavingToChecking(long savingAccountNum, long checkingAccountNum, decimal amount)
+        {
+            return _irepbank.TransferSavingToChecking(savingAccountNum, checkingAccountNum, amount, 0);
+        }
+        List<TransactionHistoryVM> IBusinessBanking.GetTransactionHistory(long checkingAccountNum)
+        {
+            return _irepbank.GetTransactionHistory(checkingAccountNum);
+        }
+
+        public bool BillPayment(long checkingAccountNum, long payeeAccountNum, decimal amount)
+        {
+            return _irepbank.BillPayment(checkingAccountNum, payeeAccountNum, amount,0);
+        }
+    }
+}
---
+++ ServicesBusiness/IBusinessBanking.cs
@@ -0,0 +1,20 @@
+﻿using BankRPEF.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.ServicesBusiness
+{
+    public interface IBusinessBanking
+    {
+        decimal GetCheckingBalance(long checkingAccountNum);
+        decimal GetSavingBalance(long savingAccountNum);
+        long GetCheckingAccountNumForUser(string username);
+        long GetSavingAccountNumForUser(string username);
+        bool TransferCheckingToSaving(long checkingAccountNum, long savingAccountNum, decimal amount);
+        bool TransferSavingToChecking(long savingAccountNum, long checkingAccountNum, decimal amount);
+        bool BillPayment(long checkingAccountNum, long payeeAccountNum, decimal amount);
+        List<TransactionHistoryVM> GetTransactionHistory(long checkingAccountNum);
+    }
+}
