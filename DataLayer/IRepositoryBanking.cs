576e9ec9b11d240a835ff62b3ac8ad437bcec207
Anastasiia01 <33880873+Anastasiia01@users.noreply.github.com>
2020-03-10 20:35:41 -0400

    Data Layer, where I access databases

---
+++ DataLayer/IRepositoryBanking.cs
@@ -0,0 +1,20 @@
+﻿using BankRPEF.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.DataLayer
+{
+    public interface IRepositoryBanking
+    {
+        decimal GetCheckingBalance(long checkingAccountNum);
+        decimal GetSavingBalance(long savingAccountNum);
+        long GetCheckingAccountNumForUser(string username);
+        long GetSavingAccountNumForUser(string username);
+        bool TransferCheckingToSaving(long checkingAccountNum, long savingAccountNum, decimal amount, decimal transactionFee);
+        bool TransferSavingToChecking(long savingAccountNum, long checkingAccountNum, decimal amount, decimal transactionFee);
+        bool BillPayment(long checkingAccountNum, long payeeAccountNum, decimal amount, decimal transactionFee);
+        List<TransactionHistoryVM> GetTransactionHistory(long checkingAccountNum);
+    }
+}
