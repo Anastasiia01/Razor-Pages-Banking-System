36d861c79429bda9bbd32a29c39d264d0902d59d
Anastasiia01 <33880873+Anastasiia01@users.noreply.github.com>
2020-03-10 20:40:52 -0400

    All razor pages needed for this web app.

---
+++ Pages/BillPayment.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankRPEF.Models;
+using BankRPEF.ServicesBusiness;
+using BankRPEF.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankRPEF.Pages
+{
+    public class BillPaymentModel : PageModel
+    {
+        IBusinessBanking _ibusbank = null;
+
+        public BillPaymentModel(IBusinessBanking ibusbank)
+        {
+            _ibusbank = ibusbank;
+        }
+        public decimal CheckingBalance { get; set; }
+
+        [BindProperty]
+        public decimal PaymentAmount { get; set; }
+
+        [BindProperty]
+        public long PayeeAccountNumber { get; set; }
+        public string Message { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (SessionFacade.USERINFO == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+            else
+            {
+                UserInfo userInfo = SessionFacade.USERINFO;
+                CheckingBalance = _ibusbank.GetCheckingBalance(userInfo.CheckingAccountNumber);
+            }
+            Message = "";
+            PaymentAmount = 0;
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (SessionFacade.USERINFO == null) // not logged in
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            else
+            {
+                UserInfo uinfo = SessionFacade.USERINFO;
+                bool ret = _ibusbank.BillPayment(uinfo.CheckingAccountNumber, PayeeAccountNumber, PaymentAmount);
+                if (ret == true)
+                {
+                    CheckingBalance = _ibusbank.GetCheckingBalance(uinfo.CheckingAccountNumber);
+                    Message = "Transaction is in the progress...Might take up to 2 days..";
+                    // clear history cache
+                    string key = String.Format("TRHISTORY_{0}", uinfo.CheckingAccountNumber);
+                    CacheAbstractionHelper.CABS.Remove(key);
+                }
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/XferStoC.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankRPEF.Models;
+using BankRPEF.Utils;
+using BankRPEF.ServicesBusiness;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankRPEF.Pages
+{
+    public class XferStoCModel : PageModel
+    {
+        IBusinessBanking _ibusbank = null;
+
+        public XferStoCModel(IBusinessBanking ibusbank)
+        {
+            _ibusbank = ibusbank;
+        }
+        public decimal SavingBalance { get; set; }
+        public decimal CheckingBalance { get; set; }
+
+        [BindProperty]
+        public decimal TransferAmount { get; set; }
+
+        public string Message { get; set; }
+
+        public IActionResult OnGet()
+        {
+
+            if (SessionFacade.USERINFO == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+            else
+            {
+                UserInfo userInfo = SessionFacade.USERINFO;
+                SavingBalance = _ibusbank.GetSavingBalance(userInfo.SavingAccountNumber);
+                CheckingBalance = _ibusbank.GetCheckingBalance(userInfo.CheckingAccountNumber);
+            }
+            Message = "";
+            TransferAmount = 0;
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (SessionFacade.USERINFO == null) // not logged in
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            else
+            {
+                UserInfo userInfo = SessionFacade.USERINFO;
+                bool res = _ibusbank.TransferSavingToChecking(userInfo.SavingAccountNumber, userInfo.CheckingAccountNumber, TransferAmount);
+                if (res)
+                {
+                        CheckingBalance = _ibusbank.GetCheckingBalance(userInfo.CheckingAccountNumber);
+                        SavingBalance = _ibusbank.GetSavingBalance(userInfo.SavingAccountNumber);
+                        Message = "Transfer succeeded..";
+                        // clear history cache
+                        string key = String.Format("TRHISTORY_{0}", userInfo.CheckingAccountNumber);
+                        CacheAbstractionHelper.CABS.Remove(key);
+                }            
+            }
+            return Page();
+        }
+    }
+}
