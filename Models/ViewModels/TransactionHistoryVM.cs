8845794c3e8dff8672b59cc0f5dc339e0b1be091
Anastasiia01 <33880873+Anastasiia01@users.noreply.github.com>
2020-03-10 20:39:35 -0400

    Helpful Classes and Identity Framework files
    I created helpful classes to encapsulate needed data as well as here are those files generated by Identity Framework that is used for security purposes.

---
+++ Models/ViewModels/TransactionHistoryVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRPEF.Models.ViewModels
+{
+    public class TransactionHistoryVM : TransactionHistories
+    {
+        public string TransactionTypeName { get; set; } // added field
+
+    }
+}
